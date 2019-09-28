2a2fc9aa46ddd522f56a5cb2528459e7e0e7afc4
Whittle <matt.whittle10@gmail.com>
2019-09-27 21:48:06 -0600

    added highscore update function

--- src/App.js
+++ src/App.js
@@ -5,4 +5,20 @@ import Header from "./components/Header";
 import cards from "./cards.json";
 import "./App.css";
 
+class App extends Component {
+  state = {
+    cards,
+    score: 0,
+    highscore: 0
+  };
+  gameover = () => {
+    if (this.state.score > this.state.highscore) {
+      this.setState({highscore: this.state.score}, function() {
+        console.log(this.state.highscore);
+      })
+    }
+  }
+}
+
+
 export default App;

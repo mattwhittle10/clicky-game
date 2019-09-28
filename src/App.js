3ea4dec6307f6d41f6f6379ecede9228621fd89d
Whittle <matt.whittle10@gmail.com>
2019-09-27 21:58:33 -0600

    function to set score equal to 0 once the game over function runs

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Wrapper from "./components/Wrapper";
 import Header from "./components/Header";
 import cards from "./cards.json";
 import "./App.css";
+import { is } from "@babel/types";
 
 class App extends Component {
   state = {
@@ -17,7 +18,15 @@ class App extends Component {
         console.log(this.state.highscore);
       })
     }
+    this.state.cards.forEach(card => {
+      card.count = 0;
+    });
+    alert(`Game Over :( \nscore: ${this.state.score}`);
+    this.setState({score: 0});
+    return true;
   }
+
+  
 }
 
 
